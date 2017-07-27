a008426b01c68b55f8ce1b0e97f299d5e46ba67d
Artem Aleksashkin <aaleksashkin@iponweb.net>
2017-07-27 13:38:04 +0300

    add readme description and fixing variables names

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,12 +1,12 @@
 from math import sqrt
 
 
-def get_roots(a, b, c):
-    discriminant = b ** 2 - 4 * a * c
+def get_roots(coefficient_a, coefficient_b, coefficient_c):
+    discriminant = coefficient_b ** 2 - 4 * coefficient_a * coefficient_c
     if discriminant < 0:
         return None, None
-    root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (-b + sqrt(discriminant)) / (2 * a)
+    root1 = (-coefficient_b - sqrt(discriminant)) / (2 * coefficient_a)
+    root2 = (-coefficient_b + sqrt(discriminant)) / (2 * coefficient_a)
     if discriminant == 0:
         return root1, None
     else:
